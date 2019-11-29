d9df995ac4fe3785d1a060c4162b853106b637cf
anna <anna.svetlov@gmail.com>
2019-11-29 18:43:35 -0500

    Bot connects to twitch and server responds on 3000

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const tmi = require("tmi.js");
+const app = express();
+const port = 3000;
+
+// Glitch expects a web server so we're starting express to take care of that.
+// The page shows the same information as the readme and includes the remix button.
+app.get("/", function (request, response) {
+  response.send("It's working!?");
+});
+
+// Setting options for our bot, disable debug output once your up and running.
+let options = {
+  identity: {
+    username: "Innovation_Bot",
+    password: "oauth:7shxj5o07b3j90nnwuey9aaim7lsec"
+  },
+    channels: ["konstantinnovation"]
+};
+
+// Set up our new TMI client and connect to the server.
+let client =  new tmi.client(options);
+client.connect();
+
+// We have debug enabled now but if not we want some sort of confirmation
+// we've connected to the server.
+client.on('connected', (address, port) => {
+  console.log(`Connected to ${address}:${port}`);
+})
+
+// Bot is listening for messages
+client.on('message', (channel, user, message, self) => {
+  switch(message) {
+    case '!test': 
+      client.action('konstantinnovation', `Replying to ${user['display-name']}`);
+      break;
+    default:
+      break;
+  }
+})
+
+let listener = app.listen(port, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
