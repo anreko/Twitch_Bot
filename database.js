3fbd70fb498312fb96f5f4d7b879b1e3d780393e
Konstantinnovation <Konstans@gmail.com>
2019-12-28 20:11:56 -0500

    fuck it

---
+++ database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : "192.168.1.14",
+  user            : 'twitchbot',
+  password        : 'm1nut35s',
+  database        : 'twitchdb'
+});
+
+module.exports = {
+  query: (text, callback) => {
+    console.log('executing query', text);
+    return pool.query(text, callback);
+  },
+};
